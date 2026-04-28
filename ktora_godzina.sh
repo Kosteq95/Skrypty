9f6290016b785aefb7e65b0c792578a6c9301625
s8@pakiso <s8@pakiso>
2026-04-28 11:39:38 +0000

    sprawdzenie minut

--- ktora_godzina.sh
+++ ktora_godzina.sh
@@ -1,24 +1,37 @@
 #! /bin/bash
 # skrypt dla korposzczura
 
-czy_odjac=0
 
 read -p "Podaj o której kończysz prace" godzina_z minuty_z
-                   #    &&
-if [ $minuty_z = "00" ] || [ -z $minuty_z ]
+
+if [ -z $minuty_z ]
 then
-    minuty_z=60
-    czy_odjac=1
+    minuty_z=0
+
 fi
+
+if ((minuty_z < 0 || minuty_z > 60))
+then
+    echo "Nie poprowanie wprowadzone minuty!" 1>&2
+    exit 1
+fi
+
 
 godzina="$(($(date +%H)+2))-$(date +%M)"
 h_now=$(($(date +%H)+2))
 m_now=$(date +%M)
 
-h_zostalo=$((godzina_z - h_now - czy_odjac))
+h_zostalo=$((godzina_z - h_now))
+
+if [ $minuty_z -lt $m_now ]
+then
+    h_zostalo=$((h_zostalo-1))
+    minuty_z=$((minuty_z+60))
+fi
+
 m_zostalo=$((minuty_z-m_now))
 
 echo "Jest aktualnie $godzina"
-echo "Zostało do końca szychty zostało $h_zostalo:$m_zostalo"
+echo "Zostało do końca szychty zostało $h_zostalo h $m_zostalo min"
 echo "test"
 echo "Test githuba"
