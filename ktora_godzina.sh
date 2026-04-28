4d307a76e44cdedc5898ea6550ea23977ea4d807
s8@pakiso <s8@pakiso>
2026-04-28 10:44:54 +0000

    ini

---
+++ ktora_godzina.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# skrypt dla korposzczura
+godzina_z=15
+godzina="$(($(date +%H)+2))-$(date +%M)"
+h_now=$(($(date +%H)+2))
+m_now=$(date +%M)
+
+h_zostalo=$(($godzina_z - $h_now - 1))
+m_zostalo=$((60-$m_now))
+
+echo "Jest aktualnie $godzina"
+echo "Zostało do końca szychty zostało $h_zostalo:$m_zostalo"
+echo "test"
