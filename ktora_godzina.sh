2f451d3e700d7fe723153a704a0f9c908f2b661e
s8@pakiso <s8@pakiso>
2026-04-28 11:14:11 +0000

    if 00

--- ktora_godzina.sh
+++ ktora_godzina.sh
@@ -1,12 +1,19 @@
 #! /bin/bash
 # skrypt dla korposzczura
-godzina_z=15
+
+read -p "Podaj o której kończysz prace" godzina_z minuty_z
+
+if [ $minuty_z = "00" ]
+then
+    minuty_z=60
+fi
+
 godzina="$(($(date +%H)+2))-$(date +%M)"
 h_now=$(($(date +%H)+2))
 m_now=$(date +%M)
 
-h_zostalo=$(($godzina_z - $h_now - 1))
-m_zostalo=$((60-$m_now))
+h_zostalo=$((godzina_z - h_now - 1))
+m_zostalo=$((minuty_z-m_now))
 
 echo "Jest aktualnie $godzina"
 echo "Zostało do końca szychty zostało $h_zostalo:$m_zostalo"
