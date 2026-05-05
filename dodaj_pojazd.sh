5d390a9fa4d03a9af758c81b7508eb1e3d84b7b1
s8@pakiso <s8@pakiso>
2026-05-05 10:50:02 +0000

    nowy skrypt pojazdy

---
+++ dodaj_pojazd.sh
@@ -0,0 +1,9 @@
+#rozdział na samochody osobowe -o  i ciężarowe -c --- podawanie przez argument -- bez argumentu to sam osobowy 
+#pobrać Markę, Model, Rocznik, Numer rej, Date ostatniego przegladu, Przebieg przy przegladzie, 
+#skrypt ma nam utworzyć bazę w ~/Baza_pojazdow/
+#                                           - osobowe
+#                                                |- omoda
+#                                           - ciężarowe
+#Skrytp ma wykryć nowy typ badź markę pojazdu i utworzyć odpowiednie foldery 
+#"Pasport pojazdu" ma być zapisany jako plik o nazwie numru rejestracyjnego np TK012345.pc
+
