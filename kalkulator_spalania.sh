c4009db48da39828d7ca479bfc2fed8573e92e01
s8@pakiso <s8@pakiso>
2026-05-19 11:53:35 +0000

    kalcV2

--- kalkulator_spalania.sh
+++ kalkulator_spalania.sh
@@ -12,9 +12,26 @@ spalanie=$(echo "scale=2; 100*$litr/$km" | bc -l)
 echo "Spanianie wyniosło: $spalanie"
 
 
-read -p "Podaj nymer rejestracyjny samochodu: " nr_rej
+read -p "Podaj numer rejestracyjny samochodu: " nr_rej
 
 
-f_dir=$(find $HOME/Baza_pojazdow/ -type f -name $nr_rej)
+f_dir=$(find $HOME/Baza_pojazdow/ -type f -name $nr_rej.pc)
+cat $f_dir
 
-echo $f_dir
+ilelinii=$(wc -l $f_dir | cut -d " " -f 1) 
+
+czyspalanie=$(grep -c Spalanie $f_dir)
+echo $czyspalanie
+
+if [ $czyspalanie -gt 0 ]
+then
+    sed -i $(($ilelinii-1))"d" $f_dir 
+    sed -i $(($ilelinii-1))"d" $f_dir 
+else
+    sed -i $ilelinii"d" $f_dir 
+fi
+
+ilelinii=$(wc -l $f_dir | cut -d " " -f 1) 
+
+sed -i $ilelinii"a  Spalanie: $spalanie" $f_dir
+sed -i "$((ilelinii+1))a  Ostania aktualizacja odbyła się  $(date +%d).$(date +%m).$(date +%Y) $(date +%R):$(date +%S) przez $USER" $f_dir
